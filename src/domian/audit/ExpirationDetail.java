f5ce845c1a2dad2754b15311b365a918f271baef
JuanDiegoTdeA <juan.millan32@correo.tdea.edu.co>
2026-03-13 18:10:13 -0500

    The main NoSQL classes and BankAccount were created

--- src/domian/audit/ExpirationDetail.java
+++ src/domian/audit/ExpirationDetail.java
@@ -1,5 +1,48 @@
 package ConstruccionDeSoftwareII.src.domian.audit;
 
+import java.time.LocalDateTime;
+
+/**
+    Member develop: Maria Jose Lopera Velasquez
+    Version 1.0
+ */
+
+
 public class ExpirationDetail {
 
+    private String reason;
+    private LocalDateTime expirationDateTime;
+    private int creatorUserID;
+
+    public ExpirationDetail(String reason,
+                            LocalDateTime expirationDateTime,
+                            int creatorUserID) {
+        this.reason = reason;
+        this.expirationDateTime = expirationDateTime;
+        this.creatorUserID = creatorUserID;
+    }
+
+    public String getReason() {
+        return reason;
+    }
+
+    public void setReason(String reason) {
+        this.reason = reason;
+    }
+
+    public LocalDateTime getExpirationDateTime() {
+        return expirationDateTime;
+    }
+
+    public void setExpirationDateTime(LocalDateTime expirationDateTime) {
+        this.expirationDateTime = expirationDateTime;
+    }
+
+    public int getCreatorUserID() {
+        return creatorUserID;
+    }
+
+    public void setCreatorUserID(int creatorUserID) {
+        this.creatorUserID = creatorUserID;
+    }
 }
